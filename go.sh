fd33deb342792192a068eabe69a1c6a75b8b4187
James Edwards <j.a.edwards86@googlemail.com>
2014-09-21 00:55:32 +0100

    moving crap outta shell scripts

--- go.sh
+++ go.sh
@@ -2,12 +2,6 @@
 apt-get -yy update
 apt-get -yy install ansible git
 
-useradd -m jedwards 
-
-mkdir /home/jedwards/.ssh
-
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEd6kguCGavHe59RpN6AxBQAZAqPWY5gMsTB7JJdsG+0A4Yh6LcxbX/DByMxfc0mCJHCTk5ShBitjToTwN946DCG9bflmYb4ZDHb7AA8df2G1zYVp3EVm1WCxx/is/ilS8nP25YCb9pgAA6eLyZxeEMYzvfLjZcANpbkf9fnCAWGnKZc8P3eTQzS4hvHAH9dqNxSo8FwGpl/deLUVzZfkkzjuCRS7I7mJR9PgYHonOg4OzS0Nh3/wGhuLw9QxF11MEDEH2D53e1ec9D1MPhODzFm1/sLYVVy6zqneXFb2yv6BQsw2xTxshKUHmtymAubW3g6DXrsEox6dPkxrJpb1b jedwards@euloan10.local" >> /home/jedwards/authorized_keys
-
 echo "[local]"  >> /var/hosts
 echo "127.0.0.1" >> /var/hosts
 ansible-pull -U https://github.com/jae2/ansible-provsion.git -i /var/hosts
