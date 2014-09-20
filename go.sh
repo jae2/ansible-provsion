4239e62fe27c35017dddc429b8a50320d5d72631
James Edwards <j.a.edwards86@googlemail.com>
2014-09-21 00:25:50 +0100

    move provisioning out of shell scripts

--- go.sh
+++ go.sh
@@ -1,7 +1,5 @@
 #/bin/sh
 apt-get -yy update
-apt-get -yy install vim
-apt-get -yy install python-httplib2
 apt-get -yy install ansible git
 
 useradd -m jedwards 
@@ -10,6 +8,6 @@ mkdir /home/jedwards/.ssh
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEd6kguCGavHe59RpN6AxBQAZAqPWY5gMsTB7JJdsG+0A4Yh6LcxbX/DByMxfc0mCJHCTk5ShBitjToTwN946DCG9bflmYb4ZDHb7AA8df2G1zYVp3EVm1WCxx/is/ilS8nP25YCb9pgAA6eLyZxeEMYzvfLjZcANpbkf9fnCAWGnKZc8P3eTQzS4hvHAH9dqNxSo8FwGpl/deLUVzZfkkzjuCRS7I7mJR9PgYHonOg4OzS0Nh3/wGhuLw9QxF11MEDEH2D53e1ec9D1MPhODzFm1/sLYVVy6zqneXFb2yv6BQsw2xTxshKUHmtymAubW3g6DXrsEox6dPkxrJpb1b jedwards@euloan10.local" >> /home/jedwards/authorized_keys
 
-echo "[local]"  >> /etc/ansible/hosts/
-echo "127.0.0.1" >> /etc/ansible/hosts/
-ansible-pull -U https://github.com/jae2/ansible-provsion.git
+echo "[local]"  >> /var/hosts
+echo "127.0.0.1" >> /var/hosts
+ansible-pull -U https://github.com/jae2/ansible-provsion.git -i /var/hosts
