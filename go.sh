69169a538b7a001d8ce31d5b4deaaa2df5db0b6d
James Edwards <james.edwards@digital.cabinet-office.gov.uk>
2014-12-16 23:17:38 +0000

    argh

--- go.sh
+++ go.sh
@@ -1,7 +1,13 @@
 #/bin/sh
-apt-get -yy update
-apt-get -yy install ansible git
+sudo apt-get -yy update
+sudo apt-get -yy install ansible git curl
 
-echo "[local]"  >> /var/hosts
-echo "127.0.0.1" >> /var/hosts
-ansible-pull -U /ansible_source/.git -i /var/hosts
+#sudo echo "[local]"  >> /var/hosts
+#sudo echo "127.0.0.1" >> /var/hosts
+#
+cd /tmp
+git clone https://github.com/jae2/ansible-provsion.git
+
+cd ansible-provsion
+
+sudo ansible-pull -U /tmp/ansible-provsion -i /var/hosts
